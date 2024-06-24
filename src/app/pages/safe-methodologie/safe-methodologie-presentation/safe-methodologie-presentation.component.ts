c31e104d4192f4334d0f2c481b63f07e57ff9d25
abdessalem <slouma4ghodbeny@gmail.com>
2024-06-24 18:08:42 +0200

    new demo

---
+++ src/app/pages/safe-methodologie/safe-methodologie-presentation/safe-methodologie-presentation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-safe-methodologie-presentation',
+  templateUrl: './safe-methodologie-presentation.component.html',
+  styleUrls: ['./safe-methodologie-presentation.component.scss']
+})
+export class SafeMethodologiePresentationComponent {
+
+}
--- src/app/pages/safe-methodologie/safe-methodologie.module.ts
+++ src/app/pages/safe-methodologie/safe-methodologie.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NbWindowModule } from '@nebular/theme';
 import { SAFeMethodologieRoutingModule } from './safe-methodologie-routing.module';
 import { DemoSAFeComponent } from './demo-safe/demo-safe.component';
  
 import { NbThemeModule, NbLayoutModule, NbCardModule, NbButtonModule, NbIconModule ,NbStepperModule,NbAccordionModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { SafeMethodologiePresentationComponent } from './safe-methodologie-presentation/safe-methodologie-presentation.component';
+ 
 @NgModule({
   declarations: [
-    DemoSAFeComponent
+    DemoSAFeComponent,
+    SafeMethodologiePresentationComponent,
+ 
   ],
   imports: [
     CommonModule,NbButtonModule,NbCardModule,NbLayoutModule,
     NbCardModule,
     NbButtonModule,NbAccordionModule ,
     NbIconModule,
-    NbEvaIconsModule,NbStepperModule ,
+    NbEvaIconsModule,NbStepperModule ,NbWindowModule,
     SAFeMethodologieRoutingModule
   ]
 })
