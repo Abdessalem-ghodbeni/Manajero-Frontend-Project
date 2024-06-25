8724c161ef388aa7ab3d58c52430f2de7e724842
balkis hmidi <balkis.hmidi@esprit.tn>
2024-06-26 00:41:57 +0100

    Adv-Disad

--- src/app/pages/safe-methodologie/safe-methodologie-routing.module.ts
+++ src/app/pages/safe-methodologie/safe-methodologie-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DemoSAFeComponent } from './demo-safe/demo-safe.component';
+import { SafeMethodologiePresentationComponent } from './safe-methodologie-presentation/safe-methodologie-presentation.component';
 
 const routes: Routes = [
-  {path:'agile/safe',component:DemoSAFeComponent}
+  {path:'agile/safe',component:DemoSAFeComponent},
+  {path:'agile',component:SafeMethodologiePresentationComponent}
 ];
 
 @NgModule({
