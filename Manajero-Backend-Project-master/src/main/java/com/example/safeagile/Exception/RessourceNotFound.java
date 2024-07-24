0969ab73e8f0ff17eafc74703dfc81582a43eb4e
abdessalem <slouma4ghodbeny@gmail.com>
2024-07-24 12:14:46 +0200

    backlog

--- Manajero-Backend-Project-master/src/main/java/com/example/safeagile/Exception/RessourceNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.example.safeagile.Exception;
-
-public class RessourceNotFound extends RuntimeException{
-  public RessourceNotFound(String message){
-    super(message);
-  }
-}
--- Manajero-Backend-Project-master/src/main/java/com/example/safeagile/Repositories/ISprintRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.safeagile.Repositories;
-
-import com.example.safeagile.Models.Sprint;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ISprintRepository extends MongoRepository<Sprint,String> {
-}
